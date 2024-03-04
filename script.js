c4fa444f20fbda22ebef405403b276799d501dc8
Thiago Silva <slv.thiago.dev@gmail.com>
2024-03-04 11:15:03 -0300

    scroll-suave

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function initTabNav() {
     });
   }
 }
-
 initTabNav();
 
 function initAccordion() {
@@ -39,3 +38,24 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  if (linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+  }
+}
+initScrollSuave();
